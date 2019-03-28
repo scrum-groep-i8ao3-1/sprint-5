164dbae13750d425d9a02a36335cac0f41a7fa2d
dannyoo7 <dannylanssink@outloo.com>
2019-03-28 14:41:07 +0100

    update operators

--- s2-be-mo3/operators.php
+++ s2-be-mo3/operators.php
@@ -1,21 +1,24 @@
-<form>
+<<form>
     <input type="text" name="getal1">
     <input type="text" name="getal2">
     <input type="submit">
 </form>
 <?php
-$GLOBALS['getal1'] = $_GET['getal1'];
-$GLOBALS['getal2'] = $_GET['getal2'];
-$GLOBALS['antwoord'] = $GLOBALS['getal1'] + $GLOBALS['getal2'];
-teken("+");
-$GLOBALS['antwoord'] = $GLOBALS['getal1'] - $GLOBALS['getal2'];
-teken("-");
-$GLOBALS['antwoord'] = $GLOBALS['getal1'] / $GLOBALS['getal2'];
-teken(":");
-$GLOBALS['antwoord'] = $GLOBALS['getal1'] % $GLOBALS['getal2'];
-teken("%");
-$GLOBALS['antwoord'] = $GLOBALS['getal1'] * $GLOBALS['getal2'];
-teken("x");
+error_reporting(0);
+ini_set('display_errors', 0);
+    $GLOBALS['getal1'] = $_GET['getal1'];
+    $GLOBALS['getal2'] = $_GET['getal2'];
+
+    $GLOBALS['antwoord'] = $GLOBALS['getal1'] + $GLOBALS['getal2'];
+    teken("+");
+    $GLOBALS['antwoord'] = $GLOBALS['getal1'] - $GLOBALS['getal2'];
+    teken("-");
+    $GLOBALS['antwoord'] = $GLOBALS['getal1'] / $GLOBALS['getal2'];
+    teken(":");
+    $GLOBALS['antwoord'] = $GLOBALS['getal1'] % $GLOBALS['getal2'];
+    teken("%");
+    $GLOBALS['antwoord'] = $GLOBALS['getal1'] * $GLOBALS['getal2'];
+    teken("x");
 function teken($teken){
     echo $GLOBALS['getal1']." ".$teken." ".$GLOBALS['getal2']."= ".$GLOBALS['antwoord']."<br>" ;
 }
