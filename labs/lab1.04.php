9bf3dd2b83d1d4c2a29163019dbba769684e59fc
dannyoo7 <dannylanssink@outloo.com>
2019-03-28 12:16:14 +0100

    update 1.03

--- labs/lab1.04.php
+++ labs/lab1.04.php
@@ -5,6 +5,12 @@ $engels =  "7.7";
 $rekenen = "6.7";
 $programmeren = "8.5";
 $databases = "9.4";
+$naam2 = "sophie";
+$nederlands2 = "8.7";
+$engels2 =  "8.7";
+$rekenen2 = "9.7";
+$programmeren2 = "9.5";
+$databases2 = "9.2";
 echo "<table border = '1'>
 <caption>
     <strong>Rapport</strong>
@@ -22,7 +28,17 @@ echo "<table border = '1'>
             <th>$naam</th><th>$nederlands</th><th>$engels</th>
 <th>$rekenen</th><th>$programmeren</th>
 <th>$databases</th>
-<th>---------</th>
+<th>8.08</th>
+</tr>
+    </tbody>
+    </tr>
+    </thead>
+    <tbody>
+     <tr>
+            <th>$naam2</th><th>$nederlands2</th><th>$engels2</th>
+<th>$rekenen2</th><th>$programmeren2</th>
+<th>$databases2</th>
+<th>9.12</th>
 </tr>
     </tbody>
     <tfoot>
